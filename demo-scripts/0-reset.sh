c2b470d0f520642b2841fb6d2b9cb34ca8966e4a
Stephan Kraft <skraft@skraft-thinkpadt14sgen1.vienna.csb>
2025-10-20 20:44:17 +0200

    correcting scripts

--- demo-scripts/0-reset.sh
+++ demo-scripts/0-reset.sh
@@ -2,14 +2,12 @@
 
 export TUTORIAL_ROOT=/home/skraft
 export APP_DOMAIN=apps.$(kubectl get dns cluster -o jsonpath='{ .spec.baseDomain }'); echo $APP_DOMAIN
+export VAULT_ADDR=https://vault-vault.$APP_DOMAIN
 
 oc delete project workload-identity-tutorial
 echo "Workload Identity-Tutorial deleted!"
 
 kubectl apply -f resources/baseline/namespace.yaml
 
-cd $TUTORIAL_ROOT/cloudnativesecuritycon-workload-identity-tutorial
-
-kubectl apply -f resources/baseline/namespace.yaml
 
 
